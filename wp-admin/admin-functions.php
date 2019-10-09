4b2067893f6becbe9ce4a4b5143359002c37fb67
Kimberly <kim.liv.hello@gmail.com>
2019-10-08 20:12:41 -0400

    formatted footer and case studies

--- wp-admin/admin-functions.php
+++ wp-admin/admin-functions.php
@@ -10,6 +10,3 @@
  */
 
 _deprecated_file( basename( __FILE__ ), '2.5.0', 'wp-admin/includes/admin.php' );
-
-/** WordPress Administration API: Includes all Administration functions. */
-require_once( ABSPATH . 'wp-admin/includes/admin.php' );
